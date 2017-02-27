f375b1d1efefec444c7a974ae833b7f47ff6f195
Jonian Guveli <jonian@hardpixel.eu>
2017-02-27 15:04:19 +0200

    add method to set cookies

--- devdocs_desktop.py
+++ devdocs_desktop.py
@@ -25,6 +25,7 @@ class DevdocsDesktop:
 
 		self.app_url = 'https://devdocs.io'
 		self.do_link = False
+		self.cookies = None
 		self.search  = self.args.parse_args().s
 		self.session = WebKit.get_default_session()
 
@@ -100,9 +101,15 @@ class DevdocsDesktop:
 
 	def enable_persistent_cookies(self):
 		cookiefile = self.settings_path('cookies.txt')
-		cookiejar = Soup.CookieJarText.new(cookiefile, False)
-		cookiejar.set_accept_policy(Soup.CookieJarAcceptPolicy.ALWAYS)
-		self.session.add_feature(cookiejar)
+		self.cookies = Soup.CookieJarText.new(cookiefile, False)
+		self.cookies.set_accept_policy(Soup.CookieJarAcceptPolicy.ALWAYS)
+		self.session.add_feature(self.cookies)
+
+	def set_cookie(self, name, value):
+		expire = Soup.COOKIE_MAX_AGE_ONE_YEAR
+		cookie = Soup.Cookie.new(name, value, 'devdocs.io', '/', expire)
+		self.cookies.add_cookie(cookie)
+		self.cookies.save()
 
 	def update_history_buttons(self):
 		back = self.webview.can_go_back()
